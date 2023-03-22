d6799d2ab453dcc2b1fb81c651d3efa7e367f14c
felipe <felipeaugustodluz@hotmail.com>
2023-03-22 20:51:55 -0300

    subindo base

---
+++ login.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>netcar | Login </title>
+    <link rel="stylesheet" href="style.css">
+    <source src="index.js" type="text/js">
+</head>
+<body>
+    <div class="vw-100 vh-100 d-flex justify-content-center align-items-center bg-dark">
+
+        <div class="w-50 v-25 shadow bg-body-secondary rounded-3">
+            <div class="p-3">
+            <h2 class="text-center">Login</h2>
+            <div class="container text-center">
+                <div class="row">
+                    <div class="col">
+                        Senha
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        Senha
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+
+    </div>
+</body>
+</html>
