5106e6ead62d16f1db15b72f25d21af6ece4201f
felipe <felipeaugustodluz@hotmail.com>
2023-03-22 21:51:00 -0300

    Criando tela de login.

--- login.php
+++ login.php
@@ -11,22 +11,24 @@
 <body>
     <div class="vw-100 vh-100 d-flex justify-content-center align-items-center bg-dark">
 
-        <div class="w-50 v-25 shadow bg-body-secondary rounded-3">
-            <div class="p-3">
-            <h2 class="text-center">Login</h2>
-            <div class="container text-center">
-                <div class="row">
-                    <div class="col">
-                        Senha
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col">
-                        Senha
-                    </div>
-                </div>
+        <div class="w-50 v-25 p-4 shadow bg-body-secondary rounded-3">
+        
+        <form>
+            <div class="text-center">
+                <h2>Login</h2>
             </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$" placeholder="vendas@netcar.com" aria-describedby="emailHelp" required>
+            </div>  
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Senha</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="ex: Netcar@2003$_" required>
             </div>
+            <button type="submit" class="btn w-100 btn-primary">Entrar</button>
+        </form>
+        
+
         </div>
 
     </div>
