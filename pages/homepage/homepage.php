0279cee9f6a0e3e700b76a96b88f04983dbbfa5c
felipe <felipeaugustodluz@hotmail.com>
2023-04-03 19:30:54 -0300

    criando modelo para homepage e algumas alterações navbar e cruds.

--- pages/homepage/homepage.php
+++ pages/homepage/homepage.php
@@ -6,13 +6,78 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Homepage | netcar</title>
     <link rel="icon" type="image" href="../../assets/logo-minify-purple.png">
-    <link rel="stylesheet" href="homepage.css">
-    <source src="login.js" type="text/js">
+    <link rel="stylesheet" href="homepage.css">    
+    <script src="homepage.js"></script>
 </head>
 <body>
-
+    
     <?php require '../../components/nav.html';?>
 
+
+    <div class="container-fluid py-3">
+
+        <div class="row gap-2 flex justify-content-center px-6 pm-2">
+
+            <div class="col-12">
+                <form id="filter" action="" autocomplete="off" onsubmit="return false"  method="post" target="_self">                
+                    <div class="flex justify-content-center align-items-center column-gap-2 rounded gap-2">
+                        <input type="text" class="form-control shadow w-50" id="name" name="name" data-noresults-text="Nothing to see here." autocomplete="off" placeholder="Pesquise por modelo ou marca" required>                                            
+                        <div class="dropdown">
+                            <a class="btn shadow text-white bg-default" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <i class="fa-solid fa-filter opacity-2 "></i>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Ano</a></li>
+                                <li><a class="dropdown-item" href="#">Marca</a></li>
+                                <li><a class="dropdown-item" href="#">Modelo</a></li>
+                            </ul>
+                        </div>
+                        <button type="submit" id="search" class="btn shadow text-white bg-default">Buscar</button>
+                    </div>
+                </form>
+            </div> 
+
+
+            <?php 
+            $date = date("d/m/y");
+            for ($i=0; $i < 30; $i++) { 
+                echo "<div class='card col-lg-3 col-md-6 col-sm-12 p-0' style='width: 18rem;'>";
+                echo "    <img src='../../assets/cars/toyotagr.jpg' class='card-img-top w-100' alt='...'>";
+                echo "    <div class='card-body'>";
+                echo "        <a href='#'> <h5 class='card-title'>Card title</h5> </a>";
+                echo $date;
+                echo "        <p class='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p>";
+                echo "        <form id='purcharse_car' onsubmit='return false'>";
+                echo "        <div class='flex gap-2 justify-content-center'>";
+                echo "            <button type='submit' href='#' class='btn w-100 text-white btn-success'>Salvar <i class='fa-solid fa-bookmark'></i></button>";
+                echo "            <button type='submit' href='#' data-bs-toggle='modal' data-bs-target='#exampleModal' class='btn w-100 text-white bg-default'>Comprar <i class='fa-solid fa-cart-shopping'></i></button>";
+                echo "        </div>";
+                echo "        </form>";
+                echo "    </div>";
+                echo "</div>    ";
+                }
+            ?>
+              <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+        </div>
+
+    </div>
     
 </body>
 </html>
--- pages/signup/signup.php
+++ pages/signup/signup.php
@@ -7,81 +7,103 @@
     <title>netcar | Cadastro </title>
     <link rel="icon" type="image" href="../../assets/logo-minify-purple.png">
     <link rel="stylesheet" href="../../style.css">
-    <script src="signup.js"></script>
 </head>
-<body>
-    <div class="vw-100 vh-100 bg-dark">
-
+<body class="bg-dark">
+    <div class="vw-100 vh-100">
+        
         <?php require '../../components/nav.html';?>
 
-        <div class="h-100 mt-2 d-flex justify-content-center align-items-center">
+        <div class="container-fluid">
 
-            <div id="formModal" class="w-50 v-25 shadow bg-body-secondary rounded-3 p-4" style="margin-top:4rem">
-                <h3 class="text-center">Cadastro</h3>
-                <div class="grid">
-                    <form>
+            <div class="row flex align-items-center justify-content-center p-2 m-2">
+                <div class="col-lg-6 col-sm-12 shadow bg-body-secondary rounded-3 p-4">
+                    <h3 class="text-center">Cadastro</h3>
+                    <form id="signup" onsubmit="return showData()">
                         <div class="row">
-                            <div class="col-6">
-                                <label for="exampleInputNomeCompleto">Nome Completo</label>
-                                <input type="text" class="form-control" id="exampleInputNomeCompleto" aria-describedby="emailHelp" placeholder="Digite aqui seu nome completo" required>
-                            </div>
-                            <div class="col-6">
-                                <label for="dateBirth">Data de nascimento</label>
-                                <input type="date" class="form-control" id="dateBirth" aria-describedby="dateBirth" placeholder="Informe sua data de nascimento" required>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-6">                
-                                <label for="email">Endereço de email</label>
-                                <input type="email" class="form-control" id="email" aria-describedby="emailHelp" pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$" placeholder="Seu melhor email" required>                    
-                            </div>
-                            <div class="col-6">
-                                <label for="cpf">CPF</label>
-                                <input type="text" class="form-control" id="cpf" maxlength="14" aria-describedby="emailHelp" placeholder="Ex: 000.000.000-00" onkeyup="this.value = mascaraCPF(this.value)" required>
-                            </div>
-                        </div>
-    
-                        <div class="row">
-                            <div class="col-6">
-                                <label for="telefone">Telefone</label>
-                                <input type="text" class="form-control" id="telefone" maxlength="15" aria-describedby="emailHelp" placeholder="Ex: (DD) 9 9999-9999" onkeyup="this.value = mascaraTelefone(this.value)" required>
-                            </div>
-                            <div class="col-3">
-                                <label for="cep">CEP</label>
-                                <input type="text" class="form-control" id="cep" maxlength="9" onkeyup="handleZipCode(event)" aria-describedby="emailHelp" placeholder="Ex: 80000-000" required>
-                            </div>
-                            <div class="col-3">
-                                <label for="numeroResidencia">Número</label>
-                                <input type="text" class="form-control" id="numeroResidencia" maxlength="5" aria-describedby="emailHelp" placeholder="Ex: 540" required>
+                            <div class="col-md-6 col-sm-12">
+                                <label for="name">Nome Completo</label>
+                                <input type="text" class="form-control" id="name" name="name" placeholder="Digite aqui seu nome completo" required>
+                                </div>
+                                <div class="col-md-6 col-sm-12">
+                                    <label for="dateBirth">Data de nascimento</label>
+                                    <input type="date" class="form-control" id="dateBirth" name="dateBirth" placeholder="Informe sua data de nascimento" required>
+                                </div>
+                                <div class="col-md-6 col-sm-12">                
+                                    <label for="email">Endereço de email</label>
+                                    <input type="email" class="form-control" id="email" name="email" pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$" placeholder="Seu melhor email" required>                    
+                                </div>
+                                <div class="col-md-6 col-sm-12">
+                                    <label for="cpf">CPF</label>
+                                    <input type="text" class="form-control" id="cpf" name="cpf" maxlength="14" placeholder="Ex: 000.000.000-00" onkeyup="this.value = mascaraCPF(this.value)" required>
+                                </div>
+                    
+        
+                                <div class="col-md-6 col-sm-12">
+                                    <label for="telefone">Telefone</label>
+                                    <input type="text" class="form-control" id="telefone" name="telefone" maxlength="15" placeholder="Ex: (DD) 9 9999-9999" onkeyup="this.value = mascaraTelefone(this.value)" required>
+                                </div>
+                                <div class="col-md-3 col-sm-6">
+                                    <label for="cep">CEP</label>
+                                    <input type="text" class="form-control" id="cep" name="cep" maxlength="9" onkeyup="handleZipCode(event)" placeholder="Ex: 80000-000" required>
+                                </div>
+                                <div class="col-md-3 col-sm-6">
+                                    <label for="numeroResidencia">Número</label>
+                                    <input type="text" class="form-control" id="numeroResidencia" name="numeroResidencia" maxlength="5" placeholder="Ex: 540" required>
+                                </div>
+                                <div class="col-md-6 col-sm-12">
+                                    <label for="password">Senha</label>
+                                    <input type="password" class="form-control" id="password" name="password" placeholder="Senha" required>
+                                </div>              
+                                <div class="col-md-6 col-sm-12">
+                                    <label for="confirmPass">Confirmar senha</label>
+                                    <input type="password" class="form-control" id="confirmPass" name="confirmPass" placeholder="Confirmar senha" required>
+                                </div>
                             </div>
                             
-                        <div class="row">
-                            <div class="col-6">
-                                <label for="exampleInputPassword1">Senha</label>
-                                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Senha" required>
-                            </div>              
-                            <div class="col-6">
-                                <label for="exampleInputPassword2">Confirmar senha</label>
-                                <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Confirmar senha" required>
+                            <div class="col-12 mt-2 d-flex justify-content-end">
+                                <script>
+                                    const data = {};
+                                    function showData() {
+                                        const fields = ["name", "dateBirth", "email", "cpf", "telefone", "cep", "numeroResidencia","password","confirmPass"]
+                                        const form = document.forms["signup"]
+                                        
+
+                                        fields.forEach(field => {
+                                            data[field] = form[field].value;
+                                            // console.log(field + ": " + form[field].value);
+                                        })
+                                        if (data.password !== data.confirmPass) {
+                                            alert("Password dont match.")
+                                        } else {
+                                            alert(JSON.stringify(data))
+                                        }
+
+
+                                        return false;
+                                    }
+
+                                    function validPass() {
+                                        console.log(data);
+                                        if (data.password === data.confirmPass) {
+                                            return true;
+                                        }
+                                        return false;
+                                    }
+                                </script>
+                                <button class="btn bg-default text-white btn-lg w-100" id="signup-button">Cadastrar</button>
                             </div>
+                            <div class="col-12 d-flex justify-content-center">
+                                <small class="text-md font-medium text-center"><a href="../login/login.php">Possui conta ? Faça Login!</a></small>
+                                
+                            </div>
+                            
                         </div>
-    
-                        <div class="col-12 mt-2 d-flex justify-content-end">
-    
-                            <button type="submit" class="btn bg-default text-white btn-lg w-100" onclick="showData(event)">Cadastrar</button>
-                        </div>
-                        <div class="col-12 d-flex justify-content-center">
-                            <small class="text-md font-medium text-center"><a href="http://localhost/netcar/pages/login/login.php">Possui conta ? Faça Login!</a></small>
-    
-                        </div>
-                        
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
-            </div>
+                <script type="text/javascript" src="signup.js"></script>
+            
         </div>
-
-
     </div>
 </body>
 </html>
